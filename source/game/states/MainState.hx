6d4a6eca984334f5d977ff3e324296fbd7cfb8c5
HackDev <80620538+Hackx2@users.noreply.github.com>
2024-03-21 19:04:25 +0000

    FUCK

--- source/game/states/MainState.hx
+++ source/game/states/MainState.hx
@@ -1,8 +1,10 @@
 package game.states;
 
+import flixel.ui.FlxButton;
+
 class MainState extends MusicBeatState {
     private var BG:FlxSprite;
-    private var BG:FlxSprite;
+    private var doShit:FlxSprite;
     
     override function create()
         {
@@ -14,7 +16,22 @@ class MainState extends MusicBeatState {
             BG.color = 0xffffb87e;
             add(BG);
 
+            var doShit:FlxButton = new FlxButton(0, 600, "Reload Char", function()
+                {
+                    tracev2("lmao", 'add');
+                });
+            doShit.screenCenter(X);
+            doShit.scale.set(2.4,2.4);
+            add(doShit);
+        }
+        override function update(elapsed:Float)
+            {
+                super.update(elapsed);
+            }           
 
-            
+        public static function tracev2(what:String, type:String = 'add') {
+            if(what == null || type == null) return;
+            Reflect.field(FlxG.log, type)(what);
+            trace(what);
         }
 }
