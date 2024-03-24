43774be2d9efa863681e84d240e5d6fe43eb4180
HackDev <80620538+Hackx2@users.noreply.github.com>
2024-03-24 10:55:05 +0000

    fr

--- source/game/states/MainState.hx
+++ source/game/states/MainState.hx
@@ -1,5 +1,6 @@
 package game.states;
 
+import backend.data.EngineData;
 import openfl.events.ProgressEvent;
 import openfl.net.URLRequest;
 import openfl.events.IOErrorEvent;
@@ -14,13 +15,19 @@ class MainState extends MusicBeatState
 {
 	private var BG:FlxSprite;
 	private var doShit:FlxSprite;
+    var logoBl:FlxSprite;
 
-	private var downloadURL:String = "https://github.com/Hackx2/FNF-AstroEngine/releases/download/0.2.1/AstroEngine.32bit.zip";
-	
-    private var laSexyBar:FlxSprite;
-    private var urlRequest:URLRequest;
+	private var downloadURL:String;
 
-    override function create()
+	private var laSexyBar:FlxSprite;
+	private var urlRequest:URLRequest;
+
+	// Download Shit
+	private var bit:Float = 64;
+
+	private static var newVersion:String = "1.9.9";
+
+	override function create()
 	{
 		BG = new FlxSprite(-80).loadGraphic(backend.utils.Paths.image('menuDesat', 'shared'));
 		BG.scrollFactor.set(0, 0);
@@ -32,37 +39,78 @@ class MainState extends MusicBeatState
 
 		var doShit:FlxButton = new FlxButton(0, 600, "Download", onClick);
 		doShit.screenCenter(X);
-        doShit.scale.set(2.4, 2.4);
+		doShit.scale.set(2.4, 2.4);
 		add(doShit);
 
-        laSexyBar = new FlxSprite((FlxG.width - 200) / 2, (FlxG.height - 20) / 2);
-        laSexyBar.makeGraphic(200, 20, 0xFF000000);
-        laSexyBar.scale.x = 0;
-        laSexyBar.visible = false;
-        add(laSexyBar);
+		laSexyBar = new FlxSprite((FlxG.width - 200) / 2, (FlxG.height - 20) / 2);
+		laSexyBar.makeGraphic(200, 20, 0xFF000000);
+		laSexyBar.scale.x = 0;
+		laSexyBar.visible = false;
+		add(laSexyBar);
+
+        logoBl = new FlxSprite();
+		logoBl.frames = backend.utils.Paths.getSparrowAtlas('logoBumpin', 'shared');
+		logoBl.animation.addByPrefix('bump', 'logo bumpin', 24, true);
+		logoBl.animation.play('bump');
+        logoBl.scale.set(0.7,0.7);
+		logoBl.updateHitbox();
+        logoBl.antialiasing = true;
+        logoBl.setPosition(FlxG.width- logoBl.width, -50);
+		add(logoBl);
+
 	}
 
-    private function onClick():Void {
-        var fileRef:FileReference = new FileReference();
-        urlRequest = new URLRequest(downloadURL);
-        fileRef.addEventListener(Event.COMPLETE, onLoadComplete);
-        fileRef.addEventListener(ProgressEvent.PROGRESS, onProgress);
+    override function update(elapsed:Float)
+        super.update(elapsed);
 
-        fileRef.download(urlRequest, "game.zip");
-        tracev2("Downloading >w<");
-    }
+	private function onClick():Void
+	{
+		var fileRef:FileReference = new FileReference();
+		urlRequest = new URLRequest(downloadURL);
+		fileRef.addEventListener(Event.COMPLETE, onLoadComplete);
+		fileRef.addEventListener(ProgressEvent.PROGRESS, onProgress);
 
-    private function onProgress(event:ProgressEvent):Void {
+		fileRef.download(urlRequest, "game.zip");
+		tracev2("Downloading >w<");
+	}
 
-        var fuck:Float = event.bytesLoaded / event.bytesTotal;
+	private function onProgress(event:ProgressEvent):Void
+	{
+		var fuck:Float = event.bytesLoaded / event.bytesTotal;
 
-        laSexyBar.visible = true;
-        laSexyBar.scale.x = fuck;
-        tracev2('Download Progress: ${Math.round(fuck * 100)}%');
-    }
+		laSexyBar.visible = true;
+		laSexyBar.scale.x = fuck;
+		tracev2('Download Progress: ${Math.round(fuck * 100)}%');
+	}
 
-    private function onLoadComplete(event:Event):Void {
-        laSexyBar.visible = false;
-        tracev2("Download Completed >w<");
-   }
+	private function onLoadComplete(event:Event):Void
+	{
+		laSexyBar.visible = false;
+		tracev2("Download Completed >w<");
+	}
+
+
+	public function new()
+	{
+		super();
+
+		var http = new haxe.Http("https://raw.githubusercontent.com/Hackx2/FNF-AstroEngine/main/gitVersion.txt");
+
+		http.onData = function(data:String)
+		{
+			newVersion = data.split('\n')[0].trim();
+			trace('FUCK: $newVersion  | $data');
+			var curVersion:String = EngineData.mainCoreShit.coreVersion.trim();
+			trace('version online: ' + newVersion + ', your version: ' + curVersion);
+		}
+
+		http.onError = function(error)
+		{
+			trace('error: $error');
+		}
+
+		http.request();
+
+		downloadURL = 'https://github.com/Hackx2/FNF-AstroEngine/releases/download/${newVersion}/AstroEngine.${bit}bit.zip';
+	}
 }
